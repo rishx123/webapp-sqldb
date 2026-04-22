05866d8aef27621baf8c200d1fb4b25d820cc7e0
rishx123 <rishithasj21@gmail.com>
2026-04-22 06:11:02 +0000

    format

--- task06/locals.tf
+++ task06/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  name_prefix = "cmaz-lye32no2-mod6"
+  # Core names
+  rg_name         = format("%s-rg", var.name_prefix)
+  sql_server_name = format("%s-sql", var.name_prefix)
+  sql_db_name     = format("%s-sql-db", var.name_prefix)
+  asp_name        = format("%s-asp", var.name_prefix)
+  app_name        = format("%s-app", var.name_prefix)
 
-  rg_name         = "${local.name_prefix}-rg"
-  sql_server_name = "${local.name_prefix}-sql"
-  sql_db_name     = "${local.name_prefix}-sql-db"
-  asp_name        = "${local.name_prefix}-asp"
-  app_name        = "${local.name_prefix}-app"
 }
